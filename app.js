9d06e1ac5d860733873bcb0dd48bfc08b21fd7c8
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-02-22 10:56:23 +0300

    task done

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+function censor() {
+	const pairs = [];
+	return function (str1, str2 = '') {
+		if (str2) {
+			pairs.push([str1, str2]);
+		} else {
+			console.log(pairs);
+			for (let pair of pairs) {
+				str1 = str1.replace(new RegExp(pair[0], 'ig'), pair[1]);
+			}
+		}
+		return str1;
+	};
+}
 const changeScene = censor();
 
 changeScene('PHP', 'JS');
 
 changeScene('backend', 'frontend');
+
+changeScene('most', 'tower');
 
 console.log(changeScene('PHP is the most popular programming language for backend web-development')); // должно распечатать 'JS is the most popular programming language for frontend web-development'
